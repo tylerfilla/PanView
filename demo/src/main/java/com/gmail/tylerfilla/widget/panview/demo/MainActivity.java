bd327c68b63549b8ac60294987e7ab5efa303c60
Tyler Filla <tylerfilla@gmail.com>
2016-05-23 17:48:05 -0500

    Added main demo activity

---
+++ demo/src/main/java/com/gmail/tylerfilla/widget/panview/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gmail.tylerfilla.widget.panview.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inflate main layout and set as content view
+        setContentView(R.layout.activity_main);
+    }
+
+}
